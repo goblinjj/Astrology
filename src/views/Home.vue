2bfc3ed569c2a555079b7bb230923bf495b2b739
Gobling <goblinjj@aliyun.com>
2025-04-12 07:08:49 +0000

    星耀特性

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,29 +7,31 @@ const curTitle = ref({})
 </script>
 
 <template>
-  <main style="margin: 0 auto; max-width: 760px">
-    <div v-for="(c, k) in formatStar" class="frame">
-        <div class="title">{{ k }}</div>
-        <div class="parent">
-            <span v-for="s in c" class="star" @click="curTitle = s">{{ s.title }}</span>  
+    <main style="margin: 0 auto; max-width: 760px">
+        <div v-for="(c, k) in formatStar" class="frame">
+            <div class="title">{{ k }}</div>
+            <div class="parent">
+                <span v-for="s in c" class="star" @click="curTitle = s">{{ s.title }}</span>
+            </div>
         </div>
-    </div>
-    <pop v-model="curTitle"></pop>
-  </main>
+        <pop v-model="curTitle"></pop>
+    </main>
 </template>
 
 <style scoped>
 .frame {
     padding: 0.5em;
 }
+
 .title {
     font-size: large;
     margin-bottom: 0.5em;
     white-space: nowrap;
 }
+
 .star {
     padding: 0.5em;
     display: inline-block;
-    white-space: nowrap
+    white-space: nowrap;
 }
 </style>
--- src/views/Popup.vue
+++ src/views/Popup.vue
@@ -5,18 +5,18 @@ const props = defineProps(['modelValue'])
 const title = ref('')
 const curGong = ref('')
 const gong = [
-    '命宫', 
-    '兄弟宫', 
-    '夫妻宫', 
-    '子女宫', 
-    '财帛宫', 
-    '疾厄宫', 
-    '迁移宫', 
-    '交友宫', 
-    '官禄宫', 
-    '田宅宫', 
-    '福禄宫', 
-    '父母宫', 
+    '命宫',
+    '兄弟宫',
+    '夫妻宫',
+    '子女宫',
+    '财帛宫',
+    '疾厄宫',
+    '迁移宫',
+    '交友宫',
+    '官禄宫',
+    '田宅宫',
+    '福禄宫',
+    '父母宫',
 ]
 
 const emits = defineEmits(['update:modelValue'])
@@ -28,7 +28,7 @@ const hidden = () => {
 
 const formatText = (text) => {
     return text.replace(/\/(.*?)\//g, (m, p1) => {
-        return '<span style="color: red;">'+p1+'</span>'
+        return '<span style="color: red;">' + p1 + '</span>'
     })
 }
 
@@ -50,11 +50,12 @@ onMounted(() => {
         <div class="text" v-html="formatText(props.modelValue.all)" />
 
         <div class="all">
-            <div class="text" style="margin-bottom: 2em; color: cornflowerblue;"
-            v-if="props.modelValue[curGong]" 
-            v-html = formatText(props.modelValue[curGong])
-            />
-            <span v-for="g in gong" class="s" :class="{select: curGong === g}" @click="curGong = g">{{ g }}</span>
+            <div class="text" style="margin-bottom: 2em; color: cornflowerblue;" v-if="props.modelValue[curGong]"
+                v-html=formatText(props.modelValue[curGong]) />
+            <span v-for="g in gong">
+                <span v-if="props.modelValue[g]" class="s" :class="{ select: curGong === g }" @click="curGong = g">{{ g
+                }}</span>
+            </span>
         </div>
     </div>
 </template>
@@ -68,26 +69,31 @@ onMounted(() => {
     top: 0;
     left: 0;
 }
+
 @media (min-width: 1024px) {
-  /* .frame  {
+    /* .frame  {
     margin: 0 auto;
     max-width: 760px;
   } */
 }
+
 .title {
     font-size: x-large;
     padding: 0.3em;
 }
+
 .s {
     padding: 0.5em;
     width: 25%;
     display: inline-block;
     text-align: center;
 }
+
 .s.select {
     background: gray;
     color: white;
 }
+
 .text {
     padding: 0.5em;
     font-size: large;
@@ -95,6 +101,7 @@ onMounted(() => {
     max-height: 35%;
     overflow: auto;
 }
+
 .all {
     position: absolute;
     bottom: 5em;
